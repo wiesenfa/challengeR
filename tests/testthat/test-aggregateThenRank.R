1bfa8f39ea5dd2b084716b1fab43bc054bfd0055
Matthias Eisenmann <m.eisenmann@dkfz-heidelberg.de>
2020-04-06 17:28:41 +0200

    Test that aggregate-than-rank handles ties for two algorithms and one case

--- tests/testthat/test-aggregateThenRank.R
+++ tests/testthat/test-aggregateThenRank.R
@@ -59,3 +59,57 @@ test_that("aggregate-than-rank by mean works with two algorithms (reverse order)
 
   expect_equal(ranking$mat, expectedRanking)
 })
+
+test_that("aggregate-than-rank by mean works with two algorithms for one case and 'min' as ties method", {
+  data <- rbind(
+    data.frame(algo="A1", value=0.6, case="C1"),
+    data.frame(algo="A2", value=0.6, case="C1"))
+
+  challenge <- as.challenge(data, algorithm="algo", case="case", value="value", smallBetter = TRUE)
+
+  ranking <- challenge%>%aggregateThenRank(FUN = mean, ties.method = "min")
+
+  expectedRanking <- rbind(
+    "A1" = data.frame(value_FUN = 0.6, rank = 1),
+    "A2" = data.frame(value_FUN = 0.6, rank = 1))
+
+  expect_equal(ranking$mat, expectedRanking)
+})
+
+test_that("aggregate-than-rank by mean works with two algorithms for one case and 'max' as ties method", {
+  data <- rbind(
+    data.frame(algo="A1", value=0.6, case="C1"),
+    data.frame(algo="A2", value=0.6, case="C1"))
+
+  challenge <- as.challenge(data, algorithm="algo", case="case", value="value", smallBetter = TRUE)
+
+  ranking <- challenge%>%aggregateThenRank(FUN = mean, ties.method = "max")
+
+  expectedRanking <- rbind(
+    "A1" = data.frame(value_FUN = 0.6, rank = 2),
+    "A2" = data.frame(value_FUN = 0.6, rank = 2))
+
+  expect_equal(ranking$mat, expectedRanking)
+})
+
+test_that("aggregate-than-rank raises error for invalid ties method", {
+  data <- rbind(
+    data.frame(algo="A1", value=0.6, case="C1"),
+    data.frame(algo="A2", value=0.6, case="C1"))
+
+  challenge <- as.challenge(data, algorithm="algo", case="case", value="value", smallBetter = TRUE)
+
+  expect_error(challenge%>%aggregateThenRank(FUN = mean, ties.method = "maxx"),
+               "'arg' should be one of \"average\", \"first\", \"last\", \"random\", \"max\", \"min\"", fixed = TRUE)
+})
+
+test_that("aggregate-than-rank raises error for invalid ties method even when no ties present", {
+  data <- rbind(
+    data.frame(algo="A1", value=0.6, case="C1"),
+    data.frame(algo="A2", value=0.8, case="C1"))
+
+  challenge <- as.challenge(data, algorithm="algo", case="case", value="value", smallBetter = TRUE)
+
+  expect_error(challenge%>%aggregateThenRank(FUN = mean, ties.method = "maxx"),
+               "'arg' should be one of \"average\", \"first\", \"last\", \"random\", \"max\", \"min\"", fixed = TRUE)
+})
