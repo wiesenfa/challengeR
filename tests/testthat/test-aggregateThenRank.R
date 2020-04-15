e83e46094a7de72cf8b663dad798458c8637907d
Matthias Eisenmann <m.eisenmann@dkfz-heidelberg.de>
2020-04-15 19:57:38 +0200

    Test NA treatment options for aggregate-than-rank

--- tests/testthat/test-aggregateThenRank.R
+++ tests/testthat/test-aggregateThenRank.R
@@ -174,3 +174,90 @@ test_that("aggregate-than-rank by mean works with one algorithm for one case", {
 
   expect_equal(ranking$mat, expectedRanking)
 })
+
+test_that("aggregate-than-rank raises error when no NA treatment specified but NAs are contained", {
+  data <- rbind(
+    data.frame(algo="A1", value=NA, case="C1"),
+    data.frame(algo="A2", value=0.8, case="C1"))
+
+  challenge <- as.challenge(data, algorithm="algo", case="case", value="value", smallBetter = FALSE)
+
+  expect_error(challenge%>%aggregateThenRank(FUN = mean),
+               "argument \"na.treat\" is missing, with no default", fixed = TRUE)
+})
+
+test_that("aggregate-than-rank raises error when invalid NA treatment specified and NAs are contained", {
+  data <- rbind(
+    data.frame(algo="A1", value=NA, case="C1"),
+    data.frame(algo="A2", value=0.8, case="C1"))
+
+  challenge <- as.challenge(data, algorithm="algo", case="case", value="value", smallBetter = FALSE)
+
+  expect_error(challenge%>%aggregateThenRank(FUN = mean, na.treat = "na.rmx"),
+               "argument \"na.treat\" is missing, with no default", fixed = TRUE)
+})
+
+test_that("specified NA treatment does not influence ranking when no NAs are contained", {
+  data <- rbind(
+    data.frame(algo="A1", value=0.6, case="C1"),
+    data.frame(algo="A2", value=0.8, case="C1"))
+
+  challenge <- as.challenge(data, algorithm="algo", case="case", value="value", smallBetter = FALSE)
+
+  ranking <- challenge%>%aggregateThenRank(FUN = mean, na.treat = 0)
+
+  expectedRanking <- rbind(
+    "A1" = data.frame(value_FUN = 0.6, rank = 2),
+    "A2" = data.frame(value_FUN = 0.8, rank = 1))
+
+  expect_equal(ranking$mat, expectedRanking)
+})
+
+test_that("NAs are replaced by numeric value", {
+  data <- rbind(
+    data.frame(algo="A1", value=NA, case="C1"),
+    data.frame(algo="A2", value=0.8, case="C1"))
+
+  challenge <- as.challenge(data, algorithm="algo", case="case", value="value", smallBetter = FALSE)
+
+  ranking <- challenge%>%aggregateThenRank(FUN = mean, na.treat = 0)
+
+  expectedRanking <- rbind(
+    "A1" = data.frame(value_FUN = 0.0, rank = 2),
+    "A2" = data.frame(value_FUN = 0.8, rank = 1))
+
+  expect_equal(ranking$mat, expectedRanking)
+})
+
+test_that("NAs are replaced by function value", {
+  data <- rbind(
+    data.frame(algo="A1", value=NA, case="C1"),
+    data.frame(algo="A2", value=0.8, case="C1"))
+
+  replacementFunction <- function(x) { -1 }
+
+  challenge <- as.challenge(data, algorithm="algo", case="case", value="value", smallBetter = FALSE)
+
+  ranking <- challenge%>%aggregateThenRank(FUN = mean, na.treat = replacementFunction)
+
+  expectedRanking <- rbind(
+    "A1" = data.frame(value_FUN = -1.0, rank = 2),
+    "A2" = data.frame(value_FUN = 0.8, rank = 1))
+
+  expect_equal(ranking$mat, expectedRanking)
+})
+
+test_that("NAs are removed", {
+  data <- rbind(
+    data.frame(algo="A1", value=NA, case="C1"),
+    data.frame(algo="A2", value=0.8, case="C1"))
+
+  challenge <- as.challenge(data, algorithm="algo", case="case", value="value", smallBetter = FALSE)
+
+  ranking <- challenge%>%aggregateThenRank(FUN = mean, na.treat = "na.rm")
+
+  expectedRanking <- rbind(
+    "A2" = data.frame(value_FUN = 0.8, rank = 1))
+
+  expect_equal(ranking$mat, expectedRanking)
+})
