78d58ceca19353c4b63ca7a6c263211f03b4c515
Matthias Eisenmann <m.eisenmann@dkfz-heidelberg.de>
2020-05-05 18:10:39 +0200

    Test that rank-then-aggregate by median works with two algorithms for two cases

--- tests/testthat/test-rankThenAggregate.R
+++ tests/testthat/test-rankThenAggregate.R
@@ -143,3 +143,21 @@ test_that("rank-then-aggregate by mean works with two algorithms for two cases",
   expect_equal(ranking$mat, expectedRanking)
 })
 
+test_that("rank-then-aggregate by median works with two algorithms for two cases", {
+  data <- rbind(
+    data.frame(algo="A1", value=0.6, case="C1"),
+    data.frame(algo="A1", value=0.4, case="C2"),
+    data.frame(algo="A2", value=0.8, case="C1"),
+    data.frame(algo="A2", value=1.0, case="C2"))
+
+  challenge <- as.challenge(data, algorithm="algo", case="case", value="value", smallBetter = TRUE)
+
+  ranking <- challenge%>%rankThenAggregate(FUN = median)
+
+  expectedRanking <- rbind(
+    "A1" = data.frame(rank_median = 1, rank = 1),
+    "A2" = data.frame(rank_median = 2, rank = 2))
+
+  expect_equal(ranking$mat, expectedRanking)
+})
+
