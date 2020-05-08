504369bbe47d971a84c71e3e01412e5bcdb453df
Matthias Eisenmann <m.eisenmann@dkfz-heidelberg.de>
2020-05-08 08:55:32 +0200

    Test that rank-then-aggregate raises error for unused NA treatment argument in multi-task challenge

--- tests/testthat/test-rankThenAggregate.R
+++ tests/testthat/test-rankThenAggregate.R
@@ -264,3 +264,23 @@ test_that("rank-then-aggregate assigns worst rank for NA in multi-task challenge
   expect_equal(ranking$matlist$T2, expectedRankingTask2)
 })
 
+test_that("rank-then-aggregate raises error for unused NA treatment argument in multi-task challenge (2 tasks in data set)", {
+  dataTask1 <- cbind(task="T1",
+                     rbind(
+                       data.frame(algo="A1", value=0.6, case="C1"),
+                       data.frame(algo="A2", value=0.8, case="C1")
+                     ))
+
+  dataTask2 <- cbind(task="T2",
+                     rbind(
+                       data.frame(algo="A1", value=NA, case="C1"),
+                       data.frame(algo="A2", value=0.4, case="C1")
+                     ))
+
+  data <- rbind(dataTask1, dataTask2)
+
+  challenge <- as.challenge(data, by="task", algorithm="algo", case="case", value="value", smallBetter = TRUE)
+
+  expect_error(challenge%>%rankThenAggregate(FUN = mean, na.treat = "na.rm"),
+               "unused argument (na.treat = \"na.rm\")", fixed = TRUE)
+})
