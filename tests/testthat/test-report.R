48c2e17887e5162fab56d7581782c519b9553c02
Matthias Eisenmann <m.eisenmann@dkfz-heidelberg.de>
2020-05-14 18:42:04 +0200

    Test that reports for multi-task data set with bootstrapping are created

--- tests/testthat/test-report.R
+++ tests/testthat/test-report.R
@@ -279,3 +279,147 @@ test_that("Word report for multi-task data set without bootstrapping is created"
   }
 
 })
+
+test_that("PDF report for multi-task data set with bootstrapping is created", {
+  dataTask1 <- cbind(task="T1",
+                     rbind(
+                       data.frame(algo="A1", value=0.8, case="C1"),
+                       data.frame(algo="A2", value=0.6, case="C1"),
+                       data.frame(algo="A3", value=0.4, case="C1"),
+                       data.frame(algo="A1", value=0.2, case="C2"),
+                       data.frame(algo="A2", value=0.1, case="C2"),
+                       data.frame(algo="A3", value=0.0, case="C2")
+                     ))
+  dataTask2 <- cbind(task="T2",
+                     rbind(
+                       data.frame(algo="A1", value=0.2, case="C1"),
+                       data.frame(algo="A2", value=0.3, case="C1"),
+                       data.frame(algo="A3", value=0.4, case="C1"),
+                       data.frame(algo="A1", value=0.7, case="C2"),
+                       data.frame(algo="A2", value=0.8, case="C2"),
+                       data.frame(algo="A3", value=0.9, case="C2")
+                     ))
+
+  data <- rbind(dataTask1, dataTask2)
+
+  challenge <- as.challenge(data, by="task", algorithm="algo", case="case", value="value", smallBetter=FALSE)
+
+  ranking <- challenge%>%aggregateThenRank(FUN=median, ties.method="min")
+
+  meanRanks <- ranking%>%consensus(method = "euclidean")
+
+  set.seed(1)
+  rankingBootstrapped <- ranking%>%bootstrap(nboot=10)
+
+  rankingBootstrapped %>%
+    report(consensus=meanRanks,
+           title="Test Challenge",
+           file="testthat_multi_task_bootstrapping",
+           format="PDF",
+           clean=TRUE,
+           open=FALSE)
+
+  expect_true(file.exists("testthat_multi_task_bootstrapping.pdf"))
+
+  # Clean up
+  if (file.exists("testthat_multi_task_bootstrapping.pdf")) {
+    file.remove("testthat_multi_task_bootstrapping.pdf")
+  }
+
+})
+
+test_that("HTML report for multi-task data set with bootstrapping is created", {
+  dataTask1 <- cbind(task="T1",
+                     rbind(
+                       data.frame(algo="A1", value=0.8, case="C1"),
+                       data.frame(algo="A2", value=0.6, case="C1"),
+                       data.frame(algo="A3", value=0.4, case="C1"),
+                       data.frame(algo="A1", value=0.2, case="C2"),
+                       data.frame(algo="A2", value=0.1, case="C2"),
+                       data.frame(algo="A3", value=0.0, case="C2")
+                     ))
+  dataTask2 <- cbind(task="T2",
+                     rbind(
+                       data.frame(algo="A1", value=0.2, case="C1"),
+                       data.frame(algo="A2", value=0.3, case="C1"),
+                       data.frame(algo="A3", value=0.4, case="C1"),
+                       data.frame(algo="A1", value=0.7, case="C2"),
+                       data.frame(algo="A2", value=0.8, case="C2"),
+                       data.frame(algo="A3", value=0.9, case="C2")
+                     ))
+
+  data <- rbind(dataTask1, dataTask2)
+
+  challenge <- as.challenge(data, by="task", algorithm="algo", case="case", value="value", smallBetter=FALSE)
+
+  ranking <- challenge%>%aggregateThenRank(FUN=median, ties.method="min")
+
+  meanRanks <- ranking%>%consensus(method = "euclidean")
+
+  set.seed(1)
+  rankingBootstrapped <- ranking%>%bootstrap(nboot=10)
+
+  rankingBootstrapped %>%
+    report(consensus=meanRanks,
+           title="Test Challenge",
+           file="testthat_multi_task_bootstrapping",
+           format="HTML",
+           clean=TRUE,
+           open=FALSE)
+
+  expect_true(file.exists("testthat_multi_task_bootstrapping.html"))
+
+  # Clean up
+  if (file.exists("testthat_multi_task_bootstrapping.html")) {
+    file.remove("testthat_multi_task_bootstrapping.html")
+  }
+
+})
+
+test_that("Word report for multi-task data set with bootstrapping is created", {
+  dataTask1 <- cbind(task="T1",
+                     rbind(
+                       data.frame(algo="A1", value=0.8, case="C1"),
+                       data.frame(algo="A2", value=0.6, case="C1"),
+                       data.frame(algo="A3", value=0.4, case="C1"),
+                       data.frame(algo="A1", value=0.2, case="C2"),
+                       data.frame(algo="A2", value=0.1, case="C2"),
+                       data.frame(algo="A3", value=0.0, case="C2")
+                     ))
+  dataTask2 <- cbind(task="T2",
+                     rbind(
+                       data.frame(algo="A1", value=0.2, case="C1"),
+                       data.frame(algo="A2", value=0.3, case="C1"),
+                       data.frame(algo="A3", value=0.4, case="C1"),
+                       data.frame(algo="A1", value=0.7, case="C2"),
+                       data.frame(algo="A2", value=0.8, case="C2"),
+                       data.frame(algo="A3", value=0.9, case="C2")
+                     ))
+
+  data <- rbind(dataTask1, dataTask2)
+
+  challenge <- as.challenge(data, by="task", algorithm="algo", case="case", value="value", smallBetter=FALSE)
+
+  ranking <- challenge%>%aggregateThenRank(FUN=median, ties.method="min")
+
+  meanRanks <- ranking%>%consensus(method = "euclidean")
+
+  set.seed(1)
+  rankingBootstrapped <- ranking%>%bootstrap(nboot=10)
+
+  rankingBootstrapped %>%
+    report(consensus=meanRanks,
+           title="Test Challenge",
+           file="testthat_multi_task_bootstrapping",
+           format="Word",
+           clean=TRUE,
+           open=FALSE)
+
+  expect_true(file.exists("testthat_multi_task_bootstrapping.docx"))
+
+  # Clean up
+  if (file.exists("testthat_multi_task_bootstrapping.docx")) {
+    file.remove("testthat_multi_task_bootstrapping.docx")
+  }
+
+})
