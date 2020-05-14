2dccd31860f1bd2f8ea2185506ced46440d46c12
Matthias Eisenmann <m.eisenmann@dkfz-heidelberg.de>
2020-05-14 12:43:19 +0200

    Remove report functionality used for single-task data sets

--- R/report.R
+++ R/report.R
@@ -1,69 +1,6 @@
 report <- function(object,...) UseMethod("report")
 report.default <- function(object, ...) stop("not implemented for this class")
 
-report.bootstrap=function(object,
-                          file,
-                          title="<Challenge name>",
-                          colors=default_colors,
-                          format="PDF",
-                          latex_engine="pdflatex",
-                          open=TRUE,...){
-
-  # Copy the report file to a temporary directory before processing it, in
-  # case we don't have write permissions to the current working dir (which
-  # can happen when deployed).
-  if (missing(file)) tempReport <- file.path(tempdir(), "report.Rmd")
-  else {
-    a=strsplit(file,"/")[[1]]
-    path=paste0(a[-length(a)],collapse="/")
-    if (path=="") tempReport=file.path(paste0(strsplit(a[length(a)],".",fixed=T)[[1]][1],".Rmd"))
-    else tempReport=file.path(path,
-                              paste0(strsplit(a[length(a)],".",fixed=T)[[1]][1],".Rmd"))
-  }
-  file.copy(file.path(system.file("appdir", package = "challengeR"),
-                      "reportSingle.Rmd"),
-            tempReport,
-            overwrite = TRUE)
-
-  # Set up parameters to pass to Rmd document
-  params <- list(
-    object=object,
-    name=title,
-    colors=colors
-  )
-
-
-  # Knit the document, passing in the `params` list, and eval it in a
-  # child of the global environment (this isolates the code in the document
-  # from the code in this app).
-   out <- render(tempReport,
-                 switch(
-                   format,
-                   PDF = pdf_document(number_sections=T,
-                                      latex_engine=latex_engine),
-                   HTML = html_document(number_sections=T),
-                   Word = word_document()
-                   ),
-                 params = params,
-                 envir = new.env(parent = globalenv()),
-                 ...
-  )
-
-  if (!missing(file)){
-    if (is.na(strsplit(file,".",fixed=T)[[1]][2])) file=paste0(file,
-                                                               ".",
-                                                               strsplit(out,".",fixed=T)[[1]][2])
-    file.rename(out, file)
-  } else file=out
-
-  file.remove(tempReport)
-
-  if (open) system(paste0('open "', file, '"'))
-
-}
-
-
-
 report.bootstrap.list=function(object,
                                consensus,
                                file,
@@ -141,79 +78,7 @@ report.bootstrap.list=function(object,
   file.remove(tempReport)
 
   if (open) system(paste0('open "', file, '"'))
-
 }
-
-
-
-########################
-
-
-report.ranked=function(object,
-                       file,
-                       title="<Challenge name>",
-                       colors=default_colors,
-                       format="PDF",
-                       latex_engine="pdflatex",
-                       open=TRUE,...){
-
-  # Copy the report file to a temporary directory before processing it, in
-  # case we don't have write permissions to the current working dir (which
-  # can happen when deployed).
-  if (missing(file)) tempReport <- file.path(tempdir(), "report.Rmd")
-  else {
-    a=strsplit(file,"/")[[1]]
-    path=paste0(a[-length(a)],collapse="/")
-    if (path=="") tempReport=file.path(paste0(strsplit(a[length(a)],
-                                                       ".",
-                                                       fixed=T)[[1]][1],".Rmd"))
-    else tempReport=file.path(path,
-                              paste0(strsplit(a[length(a)],
-                                              ".",
-                                              fixed=T)[[1]][1],".Rmd"))
-  }
-  file.copy(file.path(system.file("appdir", package = "challengeR"),
-                      "reportSingleShort.Rmd"),
-            tempReport,
-            overwrite = TRUE)
-
-  # Set up parameters to pass to Rmd document
-  params <- list(
-    object=object,
-    name=title,
-    colors=colors
-  )
-
-
-  # Knit the document, passing in the `params` list, and eval it in a
-  # child of the global environment (this isolates the code in the document
-  # from the code in this app).
-  out <- render(tempReport,
-                switch(
-                  format,
-                  PDF = pdf_document(number_sections=T,
-                                     latex_engine=latex_engine),
-                  HTML = html_document(number_sections=T),
-                  Word = word_document()
-                  ),
-                params = params,
-                envir = new.env(parent = globalenv()),...
-  )
-
-  if (!missing(file)){
-    if (is.na(strsplit(file,".",fixed=T)[[1]][2])) file=paste0(file,
-                                                               ".",
-                                                               strsplit(out,".",fixed=T)[[1]][2])
-    file.rename(out, file)
-  } else file=out
-
-  file.remove(tempReport)
-
-  if (open) system(paste0('open "', file, '"'))
-
-}
-
-
 
 report.ranked.list=function(object,
                             consensus,
@@ -294,7 +159,4 @@ report.ranked.list=function(object,
   file.remove(tempReport)
 
   if (open) system(paste0('open "', file, '"'))
-
 }
-
-
