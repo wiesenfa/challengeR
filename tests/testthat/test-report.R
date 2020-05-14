e36073bb41d71391d7b3e908fe7a5569e3b5c7ea
Matthias Eisenmann <m.eisenmann@dkfz-heidelberg.de>
2020-05-14 18:23:59 +0200

    Test that reports for multi-task data set without bootstrapping are created

--- tests/testthat/test-report.R
+++ tests/testthat/test-report.R
@@ -168,3 +168,114 @@ test_that("Word report for single-task data set with bootstrapping is created", 
   }
 
 })
+
+test_that("PDF report for multi-task data set without bootstrapping is created", {
+  dataTask1 <- cbind(task="T1",
+                     rbind(
+                       data.frame(algo="A1", value=0.8, case="C1"),
+                       data.frame(algo="A2", value=0.6, case="C1")
+                     ))
+  dataTask2 <- cbind(task="T2",
+                     rbind(
+                       data.frame(algo="A1", value=0.2, case="C1"),
+                       data.frame(algo="A2", value=0.3, case="C1")
+                     ))
+
+  data <- rbind(dataTask1, dataTask2)
+
+  challenge <- as.challenge(data, by="task", algorithm="algo", case="case", value="value", smallBetter=FALSE)
+
+  ranking <- challenge%>%aggregateThenRank(FUN=median, ties.method="min")
+
+  meanRanks <- ranking%>%consensus(method = "euclidean")
+
+  ranking %>%
+    report(consensus=meanRanks,
+           title="Test Challenge",
+           file="testthat_multi_task_no_bootstrapping",
+           format="PDF",
+           clean=TRUE,
+           open=FALSE)
+
+  expect_true(file.exists("testthat_multi_task_no_bootstrapping.pdf"))
+
+  # Clean up
+  if (file.exists("testthat_multi_task_no_bootstrapping.pdf")) {
+    file.remove("testthat_multi_task_no_bootstrapping.pdf")
+  }
+
+})
+
+test_that("HTML report for multi-task data set without bootstrapping is created", {
+  dataTask1 <- cbind(task="T1",
+                     rbind(
+                       data.frame(algo="A1", value=0.8, case="C1"),
+                       data.frame(algo="A2", value=0.6, case="C1")
+                     ))
+  dataTask2 <- cbind(task="T2",
+                     rbind(
+                       data.frame(algo="A1", value=0.2, case="C1"),
+                       data.frame(algo="A2", value=0.3, case="C1")
+                     ))
+
+  data <- rbind(dataTask1, dataTask2)
+
+  challenge <- as.challenge(data, by="task", algorithm="algo", case="case", value="value", smallBetter=FALSE)
+
+  ranking <- challenge%>%aggregateThenRank(FUN=median, ties.method="min")
+
+  meanRanks <- ranking%>%consensus(method = "euclidean")
+
+  ranking %>%
+    report(consensus=meanRanks,
+           title="Test Challenge",
+           file="testthat_multi_task_no_bootstrapping",
+           format="HTML",
+           clean=TRUE,
+           open=FALSE)
+
+  expect_true(file.exists("testthat_multi_task_no_bootstrapping.html"))
+
+  # Clean up
+  if (file.exists("testthat_multi_task_no_bootstrapping.html")) {
+    file.remove("testthat_multi_task_no_bootstrapping.html")
+  }
+
+})
+
+test_that("Word report for multi-task data set without bootstrapping is created", {
+  dataTask1 <- cbind(task="T1",
+                     rbind(
+                       data.frame(algo="A1", value=0.8, case="C1"),
+                       data.frame(algo="A2", value=0.6, case="C1")
+                     ))
+  dataTask2 <- cbind(task="T2",
+                     rbind(
+                       data.frame(algo="A1", value=0.2, case="C1"),
+                       data.frame(algo="A2", value=0.3, case="C1")
+                     ))
+
+  data <- rbind(dataTask1, dataTask2)
+
+  challenge <- as.challenge(data, by="task", algorithm="algo", case="case", value="value", smallBetter=FALSE)
+
+  ranking <- challenge%>%aggregateThenRank(FUN=median, ties.method="min")
+
+  meanRanks <- ranking%>%consensus(method = "euclidean")
+
+  ranking %>%
+    report(consensus=meanRanks,
+           title="Test Challenge",
+           file="testthat_multi_task_no_bootstrapping",
+           format="Word",
+           clean=TRUE,
+           open=FALSE)
+
+  expect_true(file.exists("testthat_multi_task_no_bootstrapping.docx"))
+
+  # Clean up
+  if (file.exists("testthat_multi_task_no_bootstrapping.docx")) {
+    file.remove("testthat_multi_task_no_bootstrapping.docx")
+  }
+
+})
