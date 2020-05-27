d58cc9b9e676ab76e2a1cbdc7c5839919adcdd3f
Matthias Eisenmann <m.eisenmann@dkfz-heidelberg.de>
2020-05-27 15:27:54 +0200

    Show task names as tick mark labels only for multi-task data set in violin plot

--- R/violin.R
+++ R/violin.R
@@ -6,7 +6,7 @@ violin.bootstrap=function(x,...){
          matlist=list(x$mat))
   names(a$bootsrappedRanks)=names(a$matlist)=""
   violin.bootstrap.list(a,...)
-  
+
 }
 
 
@@ -18,20 +18,27 @@ violin.bootstrap.list=function(x,...){
     summarise(mean=mean(value,na.rm=T),
               median=median(value,na.rm=T),
               q25=quantile(value,probs = .25,na.rm=T),
-              q75=quantile(value,probs = .75,na.rm=T))%>% 
+              q75=quantile(value,probs = .75,na.rm=T))%>%
     arrange(desc(median))
-  
+
   print(as.data.frame(ss))
-  
-  ken%>%mutate(Task=factor(.data$Task, 
+
+  xAxisText <- element_blank()
+
+  # Show task names as tick mark labels only for multi-task data set
+  if (length(x$data) > 1) {
+    xAxisText <- element_text(angle = 90, vjust = 0.5, hjust = 1)
+  }
+
+  ken%>%mutate(Task=factor(.data$Task,
                            levels=ss$Task))%>%
     ggplot(aes(Task,value))+
     geom_violin(alpha=.3,
                 color=NA,
                 fill="blue")+
-    geom_boxplot(width=0.1, 
+    geom_boxplot(width=0.1,
                  fill="white")+
-    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
+    theme(axis.text.x = xAxisText,
           legend.position = "none")+
     ylab("Kendall's tau")+
     scale_y_continuous(limits=c(min(min(ken$value),0),
@@ -44,38 +51,37 @@ kendall.bootstrap.list=function(x){
   ken=lapply(1:length(x$bootsrappedRanks),function(Task){
     id=match(rownames( x$bootsrappedRanks[[Task]]),
              rownames(x$matlist[[Task]]) )
-    sapply(x$bootsrappedRanks[[Task]], 
+    sapply(x$bootsrappedRanks[[Task]],
            function(bootSample) suppressWarnings(kendall(bootSample,
                                                          x$matlist[[Task]]$rank[id])))
   } )
   names(ken)=names((x$bootsrappedRanks))
-  
+
   if (sum(is.na(x))>0){
    cat("Bootstrap samples without variability in rankings (all algorithms ranked 1) excluded.\n Frequency of such samples by task:\n",fill = T)
     sapply(ken,function(x) sum(is.na(x)))
   }
-  
-  
+
+
   return(ken)
-  
+
 }
 
 
 density.bootstrap.list=function(x,...){
   ken=melt(kendall.bootstrap.list(x))
   colnames(ken)[2]="Task"
-  
+
   cat("\n\nSummary Kendall's tau\n")
   ss=ken%>%group_by(Task)%>%
     summarise(mean=mean(value,na.rm=T),
               median=median(value,na.rm=T),
               q25=quantile(value,probs = .25,na.rm=T),
-              q75=quantile(value,probs = .75,na.rm=T))%>% 
+              q75=quantile(value,probs = .75,na.rm=T))%>%
     arrange(desc(median))
-  
+
   print(as.data.frame(ss))
-  
+
   ggplot(ken)+
     geom_density(aes(value,fill=Task),alpha=.3,color=NA)
 }
-
