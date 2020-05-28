b2335241725b8f5822f45fba39a610c42b4e34e4
Matthias Eisenmann <m.eisenmann@dkfz-heidelberg.de>
2020-05-28 15:00:01 +0200

    Test that extraction of bootstrap ranking subset returns more algorithms then requested when ties are present

--- tests/testthat/test-subset.R
+++ tests/testthat/test-subset.R
@@ -192,3 +192,29 @@ test_that("extraction of bootstrap ranking subset returns all algorithms even wh
 
   expect_equal(rankingBootstrappedSubset$matlist$T1, expectedRankingSubset)
 })
+
+test_that("extraction of bootstrap ranking subset returns more algorithms then requested when ties are present", {
+  data <- rbind(
+    data.frame(algo="A1", value=0.8, case="C1"),
+    data.frame(algo="A2", value=0.8, case="C1"),
+    data.frame(algo="A3", value=0.8, case="C1"),
+    data.frame(algo="A1", value=0.2, case="C2"),
+    data.frame(algo="A2", value=0.2, case="C2"),
+    data.frame(algo="A3", value=0.2, case="C2"))
+
+  challenge <- as.challenge(data, taskName="T1", algorithm="algo", case="case", value="value", smallBetter=FALSE)
+
+  ranking <- challenge%>%aggregateThenRank(FUN=mean, ties.method="min")
+
+  set.seed(1)
+  rankingBootstrapped <- ranking%>%bootstrap(nboot=10)
+
+  rankingBootstrappedSubset <- subset(rankingBootstrapped, top=2)
+
+  expectedRankingSubset <- rbind(
+    "A1" = data.frame(value_FUN = 0.5, rank = 1),
+    "A2" = data.frame(value_FUN = 0.5, rank = 1),
+    "A3" = data.frame(value_FUN = 0.5, rank = 1))
+
+  expect_equal(rankingBootstrappedSubset$matlist$T1, expectedRankingSubset)
+})
