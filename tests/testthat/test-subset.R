411ef20371d2288674332562344d54a553f3746e
Matthias Eisenmann <m.eisenmann@dkfz-heidelberg.de>
2020-05-28 14:15:36 +0200

    Test that extraction of subset returns all algorithms even when more are requested

--- tests/testthat/test-subset.R
+++ tests/testthat/test-subset.R
@@ -54,3 +54,26 @@ test_that("extraction of subset raises error for multi-task data set", {
   expect_error(subset(ranking, top=2),
                "Subset of algorithms only sensible for single-task challenges.", fixed=TRUE)
 })
+
+test_that("extraction of subset returns all algorithms even when more are requested", {
+  data <- rbind(
+    data.frame(algo="A1", value=0.8, case="C1"),
+    data.frame(algo="A2", value=0.6, case="C1"),
+    data.frame(algo="A3", value=0.4, case="C1"),
+    data.frame(algo="A1", value=0.2, case="C2"),
+    data.frame(algo="A2", value=0.1, case="C2"),
+    data.frame(algo="A3", value=0.0, case="C2"))
+
+  challenge <- as.challenge(data, taskName="T1", algorithm="algo", case="case", value="value", smallBetter=FALSE)
+
+  ranking <- challenge%>%aggregateThenRank(FUN=mean, ties.method="min")
+
+  rankingSubset <- subset(ranking, top=4)
+
+  expectedRankingSubset <- rbind(
+    "A1" = data.frame(value_FUN = 0.5, rank = 1),
+    "A2" = data.frame(value_FUN = 0.35, rank = 2),
+    "A3" = data.frame(value_FUN = 0.2, rank = 3))
+
+  expect_equal(rankingSubset$matlist$T1, expectedRankingSubset)
+})
