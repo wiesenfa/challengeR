2f1d658d0e47d69817c9266e7cb21dca2c03dd57
Matthias Eisenmann <m.eisenmann@dkfz-heidelberg.de>
2020-11-04 09:47:34 +0100

    Add documentation for aggregateThenRank

--- R/wrapper.R
+++ R/wrapper.R
@@ -1,3 +1,22 @@
+#' Performs ranking via aggregate-then-rank
+#'
+#' Performs ranking by first aggregating performance values across all cases (e.g., with the mean, median or another quantile) for each algorithm.
+#' This aggregate is then used to compute a rank for each algorithm.
+#'
+#' @param object The challenge object.
+#' @param FUN The aggregation function, e.g. mean, median, min, max, function(x), quantile(x, probs=0.05).
+#' @param ties.method A string specifying how ties are treated, see \code{\link{base::rank}}.
+#'
+#' @return An S3 object of class "ranked.list" to represent a ranked assessment data set.
+#'
+#' @examples
+#'
+#' \dontrun{
+#'  aggregateThenRank(challenge, FUN = mean, ties.method="average", na.treat = 0)
+#' }
+#'
+#' @family ranking functions
+#' @export
 aggregateThenRank=function(object,FUN,ties.method = "min",...){
   object %>%
     aggregate(FUN=FUN,...) %>%
