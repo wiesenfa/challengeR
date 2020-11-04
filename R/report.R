7c5d8c07f7a00800607ad5a3e0936a23cd42ed79
Matthias Eisenmann <m.eisenmann@dkfz-heidelberg.de>
2020-11-04 18:14:33 +0100

    Add documentation for report

--- R/report.R
+++ R/report.R
@@ -1,6 +1,36 @@
 report <- function(object,...) UseMethod("report")
 report.default <- function(object, ...) stop("not implemented for this class")
 
+
+#' Generates a benchmarking report with bootstrapping results
+#'
+#' Generates a benchmarking report in PDF, HTML or Word format with bootstrapping results.
+#' It contains the rankings, plots of the raw assessment data and plots of the ranking stability.
+#' For multi-task challenges, it also contains plots of cross-task insights. If you are interested in
+#' the individual plots as separate files, set argument \code{clean} to \code{FALSE} and specify \code{fig.format}.
+#'
+#' @param object The ranked (bootstrapped) assessment data set.
+#' @param consensus The rank aggregation across tasks (consensus ranking). Only needed for a multi-task data set.
+#' @param file A string specifying the file name of the report. It allows for specifying the output file path as well,
+#'   otherwise the working directory is used. If \code{file} does not have a file extension, an extension will be automatically
+#'   added according to the output format given in \code{format}. If the argument is omitted, the report is created in a
+#'   temporary folder with file name "report".
+#' @param title A string specifying the title of the report.
+#' @param colors The color scheme that is applied to the plots.
+#' @param format A string specifying the format of the report. The options are "PDF", "HTML" or "Word".
+#' @param latex_engine A string specifying the LaTeX engine for producing PDF output. The Options are "pdflatex", "lualatex", and "xelatex".
+#' @param clean A boolean indicating whether intermediate files (e.g. individual plots) should be kept. Using \code{TRUE} will clean
+#'   intermediate files that are created during rendering.
+#' @param fig.format A vector of strings containing the file format of the figures that are not removed if \code{clean} is set to \code{FALSE}.
+#'   The options are "jpeg", "png" and "pdf",  e.g. \code{fig.format = c("jpeg", "png", "pdf")}.
+#' @param dpi A positive integer specifying the resolution of the generated plot (\code{fig.format} "jpeg" or "png") in dots per inch (DPI).
+#' @param open A boolean specifying whether the report should be opened with the default system viewer after generation.
+#' @param ... Further arguments passed to or from other functions.
+#'
+#' @return
+#'
+#' @examples
+#' @export
 report.bootstrap.list=function(object,
                                consensus,
                                file,
@@ -90,6 +120,35 @@ report.bootstrap.list=function(object,
   if (open) system(paste0('open "', file, '"'))
 }
 
+#' Generates a benchmarking report without bootstrapping results
+#'
+#' Generates a benchmarking report in PDF, HTML or Word format without bootstrapping results.
+#' It contains the rankings, plots of the raw assessment data and plots of the ranking stability.
+#' For multi-task challenges, it also contains plots of cross-task insights. If you are interested in
+#' the individual plots as separate files, set argument \code{clean} to \code{FALSE} and specify \code{fig.format}.
+#'
+#' @param object The ranked (bootstrapped) assessment data set.
+#' @param consensus The rank aggregation across tasks (consensus ranking). Only needed for a multi-task data set.
+#' @param file A string specifying the file name of the report. It allows for specifying the output file path as well,
+#'   otherwise the working directory is used. If \code{file} does not have a file extension, an extension will be automatically
+#'   added according to the output format given in \code{format}. If the argument is omitted, the report is created in a
+#'   temporary folder with file name "report".
+#' @param title A string specifying the title of the report.
+#' @param colors The color scheme that is applied to the plots.
+#' @param format A string specifying the format of the report. The options are "PDF", "HTML" or "Word".
+#' @param latex_engine A string specifying the LaTeX engine for producing PDF output. The Options are "pdflatex", "lualatex", and "xelatex".
+#' @param clean A boolean indicating whether intermediate files (e.g. individual plots) should be kept. Using \code{TRUE} will clean
+#'   intermediate files that are created during rendering.
+#' @param fig.format A vector of strings containing the file format of the figures that are not removed if \code{clean} is set to \code{FALSE}.
+#'   The options are "jpeg", "png" and "pdf",  e.g. \code{fig.format = c("jpeg", "png", "pdf")}.
+#' @param dpi A positive integer specifying the resolution of the generated plot (\code{fig.format} "jpeg" or "png") in dots per inch (DPI).
+#' @param open A boolean specifying whether the report should be opened with the default system viewer after generation.
+#' @param ... Further arguments passed to or from other functions.
+#'
+#' @return
+#'
+#' @examples
+#' @export
 report.ranked.list=function(object,
                             consensus,
                             file,
