1b86559604af54984335d5d44a16eb60cb876b08
Matthias Eisenmann <m.eisenmann@dkfz-heidelberg.de>
2020-11-16 13:21:03 +0100

    Test that blob plot for visualizing ranking stability across tasks returns one plot for multi-task data set when consensus ranking is given

--- tests/testthat/test-blobPlotStabilityAcrossTasks.R
+++ tests/testthat/test-blobPlotStabilityAcrossTasks.R
@@ -44,3 +44,35 @@ test_that("blob plot for visualizing ranking stability across tasks returns one 
   actualPlot <- stability(ranking)
   expect_is(actualPlot, "ggplot")
 })
+
+test_that("blob plot for visualizing ranking stability across tasks returns one plot for multi-task data set when consensus ranking is given", {
+  dataTask1 <- cbind(task="T1",
+                     rbind(
+                       data.frame(algo="A1", value=0.8, case="C1"),
+                       data.frame(algo="A2", value=0.6, case="C1"),
+                       data.frame(algo="A3", value=0.4, case="C1"),
+                       data.frame(algo="A1", value=0.2, case="C2"),
+                       data.frame(algo="A2", value=0.1, case="C2"),
+                       data.frame(algo="A3", value=0.0, case="C2")
+                     ))
+  dataTask2 <- cbind(task="T2",
+                     rbind(
+                       data.frame(algo="A1", value=0.2, case="C1"),
+                       data.frame(algo="A2", value=0.3, case="C1"),
+                       data.frame(algo="A3", value=0.4, case="C1"),
+                       data.frame(algo="A1", value=0.7, case="C2"),
+                       data.frame(algo="A2", value=0.8, case="C2"),
+                       data.frame(algo="A3", value=0.9, case="C2")
+                     ))
+
+  data <- rbind(dataTask1, dataTask2)
+
+  challenge <- as.challenge(data, by="task", algorithm="algo", case="case", value="value", smallBetter=FALSE)
+
+  ranking <- challenge%>%aggregateThenRank(FUN=median, ties.method="min")
+
+  meanRanks <- ranking%>%consensus(method = "euclidean")
+
+  actualPlot <- stability(ranking, ordering = names(meanRanks))
+  expect_is(actualPlot, "ggplot")
+})
