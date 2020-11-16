31a95a501028eb4d396e1b54af16bed9180ca2ea
Matthias Eisenmann <m.eisenmann@dkfz-heidelberg.de>
2020-11-16 17:31:33 +0100

    Add documentation for cluster dendrogram

--- R/dendrogram.R
+++ R/dendrogram.R
@@ -1,13 +1,33 @@
+#' @export
 dendrogram <- function(object,...) UseMethod("dendrogram")
+
+#' @export
 dendrogram.default <- function(object, ...) stop("not implemented for this class")
 
+#' Creates a cluster dendrogram
+#'
+#' Creates a cluster dendrogram from a ranked assessment data set.
+#'
+#' @param object The ranked assessment data set.
+#' @param dist A string specifying the distance measure to be used, see [relations::dissimilarity()].
+#' @param method A string specifying agglomeration method to be used, see [stats::hclust()].
+#' @param ... Further arguments passed to or from other functions.
+#'
+#' @return
+#'
+#' @examples
+#'
+#' @seealso `browseVignettes("challengeR")`
+#'
+#' @family functions to visualize cross-task insights
+#' @export
 dendrogram.ranked.list <- function(object,
                                    dist = "symdiff", #the distance measure to be used. see ?relation_dissimilarity
                                    method = "complete", #the agglomeration method to be used. see ?hclust
                                    ... # arguments passed to stats:::plot.hclust
                                    ){
   relensemble=as.relation.ranked.list(object)
-  d <- relation_dissimilarity(relensemble, 
+  d <- relation_dissimilarity(relensemble,
                               method = dist)
   clust <- hclust(d,
                   method=method)
