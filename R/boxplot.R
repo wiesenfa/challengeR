403c0cc189b47455aaa8d7170e07b3bfa5d0425f
Manuel Wiesenfarth <m.wiesenfarth@dkfz-heidelberg.de>
2020-12-03 13:55:05 +0100

    return gg object for single tasks, respect color scheme

--- R/boxplot.R
+++ R/boxplot.R
@@ -18,7 +18,6 @@
 #' @family functions to visualize assessment data
 #' @export
 boxplot.ranked.list=function(x,
-                             color="blue",
                              jitter.width=0.25,...){
   algo=attr(x$data,"algorithm")
   value=attr(x$data,"value")
@@ -31,24 +30,28 @@ boxplot.ranked.list=function(x,
   }
 
   a=lapply(1:length(x),function(id){
-    ggplot(aes_string(algo,value),data=x[[id]])+
-      geom_jitter(position=position_jitter(width=jitter.width, height=0),
-                  color=color,...)+
-      geom_boxplot(outlier.shape = NA,fill=NA)+
+    ggplot(data=x[[id]])+
+      geom_jitter(aes_string(algo,value,color=algo),
+                  position=position_jitter(width=jitter.width, height=0),
+                  ...)+
+      geom_boxplot(aes_string(algo,value),
+                   outlier.shape = NA,fill=NA)+
       ggtitle(names(x)[id]) +
-      theme(axis.text.x=element_text(angle = -90, hjust = 0)) +
+      theme(axis.text.x=element_text(angle = -90, hjust = 0),
+            legend.position="none") +
       xlab("Algorithm") +
-      ylab("Metric value")
+      ylab("Metric value") 
   })
 
   # Remove title for single-task data set
   if (length(a) == 1) {
     a[[1]]$labels$title <- NULL
+    return(a[[1]])
   } else {
     names(a) = names(x$matlist)
-  }
-  class(a) <- "ggList"
-  a
+    class(a) <- "ggList"
+    return(a)
+ }
 }
 
 boxplot.comparedRanks.list=function(x,...){
