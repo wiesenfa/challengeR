019ec7bb93923ef50ed02e4ad9424478f09db6e1
Manuel Wiesenfarth <m.wiesenfarth@dkfz-heidelberg.de>
2020-12-07 12:56:09 +0100

    remove winner.ranked

--- R/winner.R
+++ R/winner.R
@@ -1,15 +1,6 @@
 winner <- function(x,...) UseMethod("winner")
 winner.default <- function(x, ...) stop("not implemented for this class")
 
-winner.ranked <-winner.bootstrap <-function(x,...){
-  x=x$mat
- #   res=x[x$rank==1,]
-    res=x[which(x$rank==min(x$rank)),,drop=F]
- #   res[,-which(colnames(res)=="rank")]
-    res
-}
-
-
 winner.ranked.list <-winner.bootstrap.list <-function(x,...){
   lapply(x$matlist, function(z) z[which(z$rank==min(z$rank)),,drop=F])
 }
