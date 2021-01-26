8d0b5eeeee9bb7bd337414cfaaf553f73c589ae2
Matthias Eisenmann <m.eisenmann@dkfz-heidelberg.de>
2021-01-26 20:31:17 +0100

    Test that blob plot for visualizing ranking stability by algorithm returns one plot if #algorithms equals #tasks

--- tests/testthat/test-blobPlotStabilityByAlgorithm.R
+++ tests/testthat/test-blobPlotStabilityByAlgorithm.R
@@ -106,3 +106,35 @@ test_that("blob plot for visualizing ranking stability by algorithm returns a pl
   expect_is(actualPlot[[2]], "ggplot")
   expect_is(actualPlot[[3]], "ggplot")
 })
+
+test_that("blob plot for visualizing ranking stability by algorithm returns one plot if #algorithms equals #tasks", {
+  dataTask1 <- cbind(task="T1",
+                     rbind(
+                       data.frame(algo="A1", value=0.8, case="C1"),
+                       data.frame(algo="A2", value=0.6, case="C1"),
+                       data.frame(algo="A1", value=0.2, case="C2"),
+                       data.frame(algo="A2", value=0.1, case="C2")
+                     ))
+  dataTask2 <- cbind(task="T2",
+                     rbind(
+                       data.frame(algo="A1", value=0.2, case="C1"),
+                       data.frame(algo="A2", value=0.3, case="C1"),
+                       data.frame(algo="A1", value=0.7, case="C2"),
+                       data.frame(algo="A2", value=0.8, case="C2")
+                     ))
+
+  data <- rbind(dataTask1, dataTask2)
+
+  challenge <- as.challenge(data, by="task", algorithm="algo", case="case", value="value", smallBetter=FALSE)
+
+  ranking <- challenge%>%aggregateThenRank(FUN=median, ties.method="min")
+
+  set.seed(1)
+  rankingBootstrapped <- ranking%>%bootstrap(nboot=10)
+
+  meanRanks <- ranking%>%consensus(method = "euclidean")
+
+  actualPlot <- stabilityByAlgorithm(rankingBootstrapped, ordering = names(meanRanks), single = FALSE)
+  expect_is(actualPlot, "ggplot")
+})
+
