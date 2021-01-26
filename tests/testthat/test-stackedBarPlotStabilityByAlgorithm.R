eea8960f2cd0c441bdd0b88b6d93a5313cbc51ac
Matthias Eisenmann <m.eisenmann@dkfz-heidelberg.de>
2021-01-26 20:32:59 +0100

    Test that stacked bar plot for visualizing ranking stability by algorithm returns one plot if #algorithms < #tasks

--- tests/testthat/test-stackedBarPlotStabilityByAlgorithm.R
+++ tests/testthat/test-stackedBarPlotStabilityByAlgorithm.R
@@ -100,5 +100,40 @@ test_that("stacked bar plot for visualizing ranking stability by algorithm retur
   expect_is(actualPlot, "ggplot")
 })
 
+test_that("stacked bar plot for visualizing ranking stability by algorithm returns one plot if #algorithms < #tasks", {
+  dataTask1 <- cbind(task="T1",
+                     rbind(
+                       data.frame(algo="A1", value=0.8, case="C1"),
+                       data.frame(algo="A2", value=0.6, case="C1"),
+                       data.frame(algo="A1", value=0.8, case="C2"),
+                       data.frame(algo="A2", value=0.6, case="C2")
+                     ))
+  dataTask2 <- cbind(task="T2",
+                     rbind(
+                       data.frame(algo="A1", value=0.2, case="C1"),
+                       data.frame(algo="A2", value=0.3, case="C1"),
+                       data.frame(algo="A1", value=0.2, case="C2"),
+                       data.frame(algo="A2", value=0.3, case="C2")
+                     ))
+  dataTask3 <- cbind(task="T3",
+                     rbind(
+                       data.frame(algo="A1", value=0.1, case="C1"),
+                       data.frame(algo="A2", value=0.8, case="C1"),
+                       data.frame(algo="A1", value=0.1, case="C2"),
+                       data.frame(algo="A2", value=0.8, case="C2")
+                     ))
+
+  data <- rbind(dataTask1, dataTask2, dataTask3)
+
+  challenge <- as.challenge(data, by="task", algorithm="algo", case="case", value="value", smallBetter=FALSE)
+
+  ranking <- challenge%>%aggregateThenRank(FUN=median, ties.method="min")
+
+  set.seed(1)
+  rankingBootstrapped <- ranking%>%bootstrap(nboot=10)
+
+  meanRanks <- ranking%>%consensus(method = "euclidean")
+
+  actualPlot <- stabilityByAlgorithm(rankingBootstrapped, stacked = TRUE)
   expect_is(actualPlot, "ggplot")
 })
