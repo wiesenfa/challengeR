8ebf10fba330a25d49a3d58a5880068f3b212edc
Manuel Wiesenfarth <m.wiesenfarth@dkfz-heidelberg.de>
2021-01-27 11:47:16 +0100

    test only 1 tast >1 test case

--- tests/testthat/test-blobPlotStabilityByAlgorithm.R
+++ tests/testthat/test-blobPlotStabilityByAlgorithm.R
@@ -106,3 +106,35 @@ test_that("blob plot for visualizing ranking stability by algorithm returns a pl
   expect_is(actualPlot[[2]], "ggplot")
   expect_is(actualPlot[[3]], "ggplot")
 })
+
+
+
+test_that("Multi task bootstrapping, only one task with >1 test case stability plot works", {
+  dataTask1 <- cbind(task="T1",
+                     rbind(
+                       data.frame(algo="A1", value=0.8, case="C1"),
+                       data.frame(algo="A2", value=0.6, case="C1")
+                     ))
+  dataTask2 <- cbind(task="T2",
+                     rbind(
+                       data.frame(algo="A1", value=0.2, case="C1"),
+                       data.frame(algo="A2", value=0.3, case="C1"),
+                       data.frame(algo="A1", value=0.2, case="C2"),
+                       data.frame(algo="A2", value=0.3, case="C2")
+                     ))
+  dataTask3 <- cbind(task="T3",
+                     rbind(
+                       data.frame(algo="A1", value=0.1, case="C1"),
+                       data.frame(algo="A2", value=0.8, case="C1")
+                     ))
+  
+  data <- rbind(dataTask1, dataTask2, dataTask3)
+  
+  challenge <- as.challenge(data, by="task", algorithm="algo", case="case", value="value", smallBetter=FALSE)
+  
+  ranking <- challenge%>%aggregateThenRank(FUN=median, ties.method="min")
+  rankingBootstrapped <- ranking%>%bootstrap(nboot=3)
+  meanRanks <- ranking%>%consensus(method = "euclidean")
+  actualPlot <- stabilityByAlgorithm(rankingBootstrapped, ordering = names(meanRanks), single = FALSE)
+  expect_is(actualPlot, "ggplot")
+})
